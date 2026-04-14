7a17027a54de801947aeff2e7198f750c9863681
Suhail89589 <mohdsuhail8958911@gmail.com>
2026-04-14 16:08:27 +0530

    Fix Vercel edge crash and DB crash overriding CORS

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     );
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1);
+    // process.exit(1); -> Removed because it crashes Vercel Serverless functions
   }
 };
 
