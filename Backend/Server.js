b36cd077d97a54a4662c2842a6235f33481069ef
Suhail89589 <mohdsuhail8958911@gmail.com>
2026-04-14 17:01:34 +0530

    nope

--- Backend/Server.js
+++ Backend/Server.js
@@ -65,10 +65,8 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 7000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
